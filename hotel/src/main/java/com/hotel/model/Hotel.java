fba64377c94a19a5dea8ffd37b88ebec9cae2fbb
André Lucas <andrelucastic@gmail.com>
2019-04-18 13:40:12 -0300

    hotel

---
+++ hotel/src/main/java/com/hotel/model/Hotel.java
@@ -0,0 +1,36 @@
+package com.hotel.model;
+
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("hotel_collect")
+public class Hotel {
+
+    private Long id;
+    private String name;
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
