8e5f44b389e069317467d9145a86d590ee58941a
André Lucas <andrelucastic@gmail.com>
2019-04-18 14:59:51 -0300

    properties files

--- hotel/src/test/java/com/hotel/hotel/DaoTests.java
+++ hotel/src/test/java/com/hotel/hotel/DaoTests.java
@@ -1,4 +1,29 @@
 package com.hotel.hotel;
 
-public class DaoTests {
+import com.hotel.model.Hotel;
+import com.hotel.repository.HotelRepository;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import reactor.core.publisher.Mono;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DaoTests extends HotelApplicationTests {
+
+
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    @Rollback(false)
+    @Test
+    public void saveHotelTest(){
+        Hotel hotel = Mockito.mock(Hotel.class);
+
+        Mono<Hotel> monoHotel = hotelRepository.save(hotel);
+
+        assertThat(monoHotel).isNotNull();
+
+    }
 }
